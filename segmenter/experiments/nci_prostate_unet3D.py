d76806d7e1a65787a773cb1e55f99813f3520d32
Christian Baumgartner <c.baumgartner@imperial.ac.uk>
2018-06-30 19:02:00 +0200

    update

--- segmenter/experiments/nci_prostate_unet3D.py
+++ segmenter/experiments/nci_prostate_unet3D.py
@@ -1,21 +1,22 @@
-from segmenter.network_zoo import nets2D
+from segmenter.network_zoo import nets3D
 import tensorflow as tf
 import os
 import config.system as sys_config
 
-experiment_name = 'nci_prostate_unet'
+experiment_name = 'prostate_unet3D'
 
 # Model settings
-network = nets2D.unet2D
+network = nets3D.unet3D
 
 # Data settings
 data_identifier = 'nci_prostate'
 preproc_folder = os.path.join(sys_config.project_root, 'data/preproc_data/nci_prostate')
 data_root = '/usr/bmicnas01/data-biwi-01/bmicdatasets-originals/Originals/Challenge_Datasets/NCI_Prostate'
-dimensionality_mode = '2D'
-image_size = (256, 256)
-target_resolution = (0.625, 0.625)
+dimensionality_mode = '3D'
+image_size = (112, 112, 24)
+target_resolution = (1.2, 1.2, 3.0)
 nlabels = 3
+tensorboard_slice = 10
 
 # Network settings
 n0 = 32
@@ -25,8 +26,8 @@ weight_decay = 0.0
 loss_type = 'crossentropy'  # 'dice_micro'/'dice_macro'/'dice_macro_robust'/'crossentropy'
 
 # Training settings
-batch_size = 12
-n_accum_grads = 1
+batch_size = 2
+n_accum_grads = 4
 learning_rate = 1e-2
 optimizer_handle = tf.train.AdamOptimizer
 beta1=0.9
