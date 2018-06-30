6770e21d4c931183bfd39a46bcbef9eb9adc9978
Christian Baumgartner <c.baumgartner@imperial.ac.uk>
2018-06-30 18:38:43 +0200

    update

--- data/data_switch.py
+++ data/data_switch.py
@@ -8,7 +8,7 @@ def data_switch(data_identifier):
         from data.adni_data import adni_data as data_loader
     elif data_identifier == 'acdc':
         from data.acdc_data import acdc_data as data_loader
-    elif data_identifier == 'prostate_nci':
+    elif data_identifier == 'nci_prostate':
         from data.nci_prostate_data import nci_prostate_data as data_loader
     else:
         raise ValueError('Unknown data identifier: %s' % data_identifier)
